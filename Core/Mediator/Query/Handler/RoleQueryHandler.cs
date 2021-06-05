c10883ddd51c9b54fbb8e62705bec8a9d3305be1
NSSure <19178714+NSSure@users.noreply.github.com>
2021-06-05 01:00:14 -0400

    Role query handler

--- Core/Mediator/Query/Handler/RoleQueryHandler.cs
+++ Core/Mediator/Query/Handler/RoleQueryHandler.cs
@@ -69,46 +69,54 @@ namespace Blazor.Markdown.Core.Mediator.Handler
             //    .Unwind<Action, RoleModel>(x => x.)
             //    .ToListAsync();
 
-            var t = await this.RoleRepository.Collection.Aggregate().Unwind<Role, UnwoundRole>(x => x.ActionKeys).ToListAsync();
+            //var t = await this.RoleRepository.Collection.Aggregate().Unwind<Role, UnwoundRole>(x => x.ActionKeys).ToListAsync();
 
-            var test = await this.RoleRepository.Collection.Aggregate()
-                .Match(x => x.ActionKeys != null)
-                .Unwind<Role, UnwoundRole>(x => x.ActionKeys)
-                .Lookup<UnwoundRole, DAL.Entity.Action, UnwoundRole>(this.RoleRepository.Context.Action, a => a.ActionKeys, a => a.Key, a => a.Action).ToListAsync();
+            //var test = await this.RoleRepository.Collection.Aggregate()
+            //    .Match(x => x.ActionKeys != null)
+            //    .Unwind<Role, UnwoundRole>(x => x.ActionKeys)
+            //    .Lookup<UnwoundRole, DAL.Entity.Action, UnwoundRole>(this.RoleRepository.Context.Action, a => a.ActionKeys, a => a.Key, a => a.Action).ToListAsync();
 
-            var tt = await this.RoleRepository.Collection.Aggregate()
-                .Unwind<Role, UnwoundRole>(x => x.ActionKeys)
-                .Lookup<UnwoundRole, DAL.Entity.Action, UnwoundRole>(this.RoleRepository.Context.Action, a => a.ActionKeys, a => a.Key, a => a.Action)
-                .Group(uwr => uwr.Id, g => new RoleModel()
-                {
-                    Id = g.First().Id,
-                    Key = g.First().Key,
-                    Name = g.First().Name,
-                    DateAdded = g.First().DateAdded,
-                    Actions = g.Select(xx => new ActionModel()
-                    { 
-                        Id = xx.Action.Id,
-                        Name = xx.Action.Name,
-                        Key = xx.Action.Key,
-                        DateAdded = xx.Action.DateAdded
-                    })
-                    .ToList()
-                })
-                .ToListAsync();
+            //var tt = await this.RoleRepository.Collection.Aggregate()
+            //    .Unwind<Role, UnwoundRole>(x => x.ActionKeys)
+            //    .Lookup<UnwoundRole, DAL.Entity.Action, UnwoundRole>(this.RoleRepository.Context.Action, a => a.ActionKeys, a => a.Key, a => a.Action)
+            //    .Group(uwr => uwr.Id, g => new RoleModel()
+            //    {
+            //        Id = g.First().Id,
+            //        Key = g.First().Key,
+            //        Name = g.First().Name,
+            //        DateAdded = g.First().DateAdded,
+            //        Actions = g.Select(xx => new ActionModel()
+            //        { 
+            //            Id = xx.Action.Id,
+            //            Name = xx.Action.Name,
+            //            Key = xx.Action.Key,
+            //            DateAdded = xx.Action.DateAdded
+            //        })
+            //        .ToList()
+            //    })
+            //    .ToListAsync();
 
 
-//            var tt = await this.RoleRepository.Collection.Aggregate().Unwind<Role, UnwoundRole>(x => x.ActionKeys).Lookup<UnwoundRole, DAL.Entity.Action, RoleModel>(this.RoleRepository.Context.Action, a => a.ActionKeys, a => a.Key, a => new
-//            {
+            //            var tt = await this.RoleRepository.Collection.Aggregate().Unwind<Role, UnwoundRole>(x => x.ActionKeys).Lookup<UnwoundRole, DAL.Entity.Action, RoleModel>(this.RoleRepository.Context.Action, a => a.ActionKeys, a => a.Key, a => new
+            //            {
 
-//            }).Group<UnwoundRole, Guid, RoleModel>(uwr => uwr.Id, g => new RoleModel()
-//            {
-//                ActionKeys = g.Select(x => x.ActionKeys).ToList()
-//            })
-//.ToListAsync();
+            //            }).Group<UnwoundRole, Guid, RoleModel>(uwr => uwr.Id, g => new RoleModel()
+            //            {
+            //                ActionKeys = g.Select(x => x.ActionKeys).ToList()
+            //            })
+            //.ToListAsync();
+
+            List<RoleModel> _diagramModels = await this.RoleRepository.ListAll(x => new RoleModel()
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Key = x.Key,
+                DateAdded = x.DateAdded
+            });
 
             return new RoleQueryResponse()
             {
-                Roles = tt
+                Roles = _diagramModels
             };
         }
     }
